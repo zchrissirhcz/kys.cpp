b1f3cf992bd768986d4e10efd90e848e13f3f5b1
Zhuo Zhang <imzhuo@foxmail.com>
2023-09-13 00:58:26 +0800

    [feat] Create Engine and TitleScene classes

--- kys.cpp
+++ kys.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <memory>
+
+namespace kys {
+
+class Engine
+{
+public:
+    Engine()
+    {
+        std::cout << "Engine created\n";
+    }
+    ~Engine()
+    {
+        std::cout << "Good bye" << std::endl;
+    }
+};
+
+class TitleScene
+{
+public:
+    int run()
+    {
+        std::cout << "Welcome to TitleScene" << std::endl;
+        return 0;
+    }
+};
 
 class Application
 {
@@ -6,12 +32,20 @@ public:
     int run()
     {
         std::cout << "Hello, KYS" << std::endl;
+
+        std::unique_ptr<Engine> engine(new Engine);
+
+        std::unique_ptr<TitleScene> scene(new TitleScene);
+        scene->run();
+
         return 0;
     }
-};
+}; // namespace kys
+
+} // namespace
 
 int main(int argc, char** argv)
 {
-    Application app;
+    kys::Application app;
     return app.run();
 }
