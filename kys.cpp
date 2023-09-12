f790aba23e775831b8608f8797073b490bbbd9cc
Zhuo Zhang <imzhuo@foxmail.com>
2023-09-13 01:13:33 +0800

    [feat] Add Engine's member functions

--- kys.cpp
+++ kys.cpp
@@ -14,6 +14,22 @@ public:
     {
         std::cout << "Good bye" << std::endl;
     }
+
+    void setStartWindowSize(int a_width, int a_height)
+    {
+        m_start_window_width = a_width;
+        m_start_window_height = a_height;
+    }
+
+    int init()
+    {
+        std::cout << "TODO: install and use SDL here" << std::endl;
+        return 0;
+    }
+
+private:
+    int m_start_window_height;
+    int m_start_window_width;
 };
 
 class TitleScene
@@ -34,6 +50,8 @@ public:
         std::cout << "Hello, KYS" << std::endl;
 
         std::unique_ptr<Engine> engine(new Engine);
+        engine->setStartWindowSize(1280, 720);
+        engine->init();
 
         std::unique_ptr<TitleScene> scene(new TitleScene);
         scene->run();
